5e94b00c42d98454341e167953e5a7cb73b4a013
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-04-25 21:47:35 -0400

    Wrote basic models for a mailing list and subscriber.

--- postman/models.py
+++ postman/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class MailingList(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+class Subscriber(models.Model):
+    email = models.EmailField()
+    to = models.ForeignKey(MailingList)
