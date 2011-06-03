deaf37a6092ccb69f80c1f1d23f9cd79cef9acd4
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-06-03 01:32:28 -0400

    Added signup form and RequestContext for CSRF

--- views.py
+++ views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+
+from postman.forms import SubscribeForm
 
 def home(request):
     '''
-    Display basic page, home.html
+    Display home.html with mailing list subscribe form
     '''
-    return render_to_response('home.html', {})
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            # do stuff
+            # redirect
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = SubscribeForm()
+
+    return render_to_response('home.html',
+            {
+                'form': form,
+            },
+            context_instance=RequestContext(request)
+        )
 
