d43f574cbf932e017a179733fec89b7527c790c8
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-06-30 14:11:25 -0700

    Added "send message" admin action - is tested with print statement, and working. Needs error handling and a better interaction with the user on the admin.

--- postman/admin.py
+++ postman/admin.py
@@ -9,11 +9,30 @@ class SubscriptionAdmin(admin.ModelAdmin):
 class MailingListAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_on', 'updated_on',)
     search_fields = ('title', 'subscribers',)
+    filter_horizontal = ('subscribers',)
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('sender', 'subject', 'status',)
     search_fields = ('subject', 'body',)
     list_filter = ('status',)
+    actions = ['send_message']
+    def send_message(self, request, queryset):
+        from django.core.mail import send_mail
+        # for each message in queryset (could select multiple)
+        for message in queryset:
+            # iterate through recipients and send an email
+            for mailing_list in message.recipients.all():
+                for subscriber in mailing_list.subscribers.all():
+                    print "Sending %s to %s" % (message.subject,
+                            subscriber.email)
+                    #send_mail(message.subject, message.body_html,
+                    #        message.from_email, [subscriber.email],
+                    #        fail_silently=False)
+            # mark message as sent
+            message.status = 'S'
+            message.save()
+
+
 
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(Message, MessageAdmin)
