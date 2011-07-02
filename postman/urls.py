99c1b1759abdb5e90620351b66ea6acccd57885e
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-07-01 17:10:21 -0700

    Wrote unsubscribe message templates.
    Note: You need to pass render_to_response a RequestContext in order for
    the TEMPLATE_CONTEXT_PROCESSORS to give it stuff (like STATIC_URL).

--- postman/urls.py
+++ postman/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^unsubscribe/(?P<code>[0-9a-f]+)/$', 'postman.views.unsubscribe'),
+    (r'^unsubscribe/confirmation/(?P<id>\d+)/$', 'postman.views.unsubscribe_confirm'),
+    (r'^unsubscribe/error/(?P<id>\d+)/$', 'postman.views.unsubscribe_error'),
 )
--- postman/views.py
+++ postman/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import send_mail
 from postman.models import Subscription, Message
@@ -28,6 +30,30 @@ def unsubscribe(request, code):
         s = Subscription.objects.get(deactivation_code=code)
         s.subscribed = False
         s.save()
-        return HttpResponseRedirect('/newsletter/unsubscribe/confirmation/')
+        return HttpResponseRedirect('newsletter/unsubscribe/confirmation/%d' %
+                s.pk)
     except:
-        return HttpResponseRedirect('/newsletter/unsubscribe/error')
+        return HttpResponseRedirect('newsletter/unsubscribe/error/%d' % s.pk)
+
+def unsubscribe_confirm(request, id):
+    s = Subscription.objects.get(pk=id)
+    message = "%s has been removed from the Honest Appalachia mailing list." % s.email
+    return render_to_response('postman/unsubscribe_confirm.html',
+            {
+                'message': message,
+            },
+            context_instance=RequestContext(request)
+        )
+
+def unsubscribe_error(request, id):
+    try:
+        s = Subscription.objects.get(pk=id)
+        message = "An error occurred while trying to remove %s from the mailing list." % s.email
+    except:
+        message = "Something went horribly wrong."
+    return render_to_response('postman/unsubscribe_error.html',
+            {
+                'message': message,
+            },
+            context_instance=RequestContext(request)
+        )
